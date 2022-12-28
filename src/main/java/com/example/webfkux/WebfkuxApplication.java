6622d6c02be7b5d3d4255009b2ab5394db6dacee
Cristian Welter <cwelter@uolinc.com>
2022-12-28 11:18:27 -0300

    feat: init

---
+++ src/main/java/com/example/webfkux/WebfkuxApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfkux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfkuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfkuxApplication.class, args);
+	}
+
+}
